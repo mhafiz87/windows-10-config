28ab3a5cca6c2506f67bae17796ec53087d687d8
Muhammad Hafiz <mhafiz.muhamad@gmail.com>
2021-09-26 23:16:17 +0800

    <UPDATE> powershell_enable_script.bat

--- powershell_enable_script.bat
+++ powershell_enable_script.bat
@@ -1,22 +1,28 @@
 @echo off
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
+REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
-    echo Requesting Admin access...
-    goto goUAC )
-    else goto goADMIN
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
 
-:goUAC
+:UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params = %*:"=""
     echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
     exit /B
 
-:goADMIN
+:gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
+:--------------------------------------
 
 powershell -Command "& {Set-ExecutionPolicy -ExecutionPolicy ByPass -Scope CurrentUser -Force}"
