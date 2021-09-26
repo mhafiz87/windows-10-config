74d9ee4d0e549300c02a340a40a9d0d52f33e760
Muhammad Hafiz <mhafiz87@users.noreply.github.com>
2021-09-26 22:44:33 +0800

    <ADD> powershell_enable_script.bat

---
+++ powershell_enable_script.bat
@@ -0,0 +1,22 @@
+@echo off
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting Admin access...
+    goto goUAC )
+    else goto goADMIN
+
+:goUAC
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:goADMIN
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+powershell -Command "& {Set-ExecutionPolicy -ExecutionPolicy ByPass -Scope CurrentUser -Force}"
