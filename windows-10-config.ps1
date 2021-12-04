2d27f3ca68d98cf812e084110d6943f738fecd25
Muhammad Hafiz <mhafiz.muhamad@gmail.com>
2021-12-04 15:24:55 +0800

    Update windows-10-config.ps1. Add copy-item powershell profile. Remove new-item and add-content.

--- windows-10-config.ps1
+++ windows-10-config.ps1
@@ -24,7 +24,7 @@ if ((Test-Admin) -eq $false)  {
         Write-Output "Elevating did not work :("
 
     } else {
-        #                                                         vvvvv add `-noexit` here for better debugging vvvvv 
+        #                                                         vvvvv add `-noexit` here for better debugging vvvvv
         Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -shouldAssumeToBeElevated -workingDirOverride "{1}"' -f ($myinvocation.MyCommand.Definition, "$workingDirOverride"))
     }
     exit
@@ -34,9 +34,6 @@ Set-Location "$workingDirOverride"
 ##### END ELEVATE TO ADMIN #####
 
 function download_github_repo_latest_release {
-    <#
-    
-    #>
     param(
         [string]$url,
         [string]$outputFilename
@@ -101,7 +98,7 @@ function Install-Software {
         else {
             $argumentListFlag = @{}
             if ($argumentList -and ($file_installer[0] -like "*.msi")) {
-                $argumentListFlag["ArgumentList"] = "/i " + """$($file_installer[0])""" + $argumentList + " /qn" 
+                $argumentListFlag["ArgumentList"] = "/i " + """$($file_installer[0])""" + $argumentList + " /qn"
             }
             elseif ($file_installer[0] -like "*.msi") {
                 $argumentListFlag["ArgumentList"] = "/i " + """$($file_installer[0])""" + " /qn"
@@ -209,7 +206,7 @@ reg import "registryshortcutsw1064bit\disable videos folder win10 64.reg"
 reg import "registryshortcutsw1064bit\search_details_1.reg"
 reg import "registryshortcutsw1064bit\search_details_2.reg"
 
-# Disable internet explorer prompt 
+# Disable internet explorer prompt
 set-itemproperty -path "hklm:\software\microsoft\internet explorer\main" -name "disablefirstruncustomize" -value 2
 
 $password_flag = read-host "do you want to enable automatic login? [y]es or [n]o"
@@ -329,272 +326,11 @@ Install-Module oh-my-posh -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
 
 # Powershell Configuration File
-New-Item -Path $HOME\documents\windowspowershell\microsoft.powershell_profile.ps1 -ItemType File
-Add-Content $HOME\documents\windowspowershell\microsoft.powershell_profile.ps1 @'
-using namespace System.Management.Automation
-using namespace System.Management.Automation.Language
-
-Function touch {
-    <#
-        .SYNOPSIS
-        Create file similar to linux touch command.
-        .DESCRIPTION
-        Create file similar to linux touch command. If the file exist, update it's last modified.
-        .PARAMETER filename
-        The name of the file to be created.
-        .EXAMPLE    
-        To create a file name "test.txt"
-        PS> touch -filename test.txt
-        .EXAMPLE    
-        PS> touch -filename README.md
-        To create a file name "README.md"
-        .LINK
-        https://superuser.com/questions/502374/equivalent-of-linux-touch-to-create-an-empty-file-with-powershell
-    #>
-    param(
-        [Parameter(Mandatory = $true)][string]$filename
-    )
-    if ($filename -eq $null) {
-        throw "No filename supplied"
-    }
-
-    if (Test-Path $file) {
-        (Get-ChildItem $file).LastWriteTime = Get-Date
-    }
-    else {
-        New-Item -Path . -ItemType File -Name $file
-    }
-}
-
-Function workon {
-    <#
-        .SYNOPSIS
-        Using "workon" to activate python virtual environment in powershell.
-        .DESCRIPTION
-        Using "workon" to activate python virtual environment in powershell. "WORKON_HOME" environment variable must be set to use this function.
-        .PARAMETER envName
-        The python virtual environment name to activate.
-        .EXAMPLE
-        PS> workon orbital
-        To activate virtual environment name "orbital"
-        .EXAMPLE
-        PS> workon machine_vision
-        To activate virtual environment name "machine_vision"
-        .LINK
-        https://stackoverflow.com/questions/38944525/workon-command-doesnt-work-in-windows-powershell-to-activate-virtualenv
-    #>
-    param(
-        [Parameter(Mandatory = $true)][string]$envName
-    )
-    & $env:WORKON_HOME\$envName\Scripts\activate.ps1
-}
-
-Function Add-Env-Variable {
-    <#
-        .SYNOPSIS
-        Add new Windows 10 environment variable.
-        .DESCRIPTION
-        Add new Windows 10 environment variable. If environment already exist, will append. Else will create new.
-        .PARAMETER envName
-        The name of the environment variable.
-        .PARAMETER userType
-        To add environment variable to user or system(machine).
-        .PARAMETER newEnv
-        The path of the environment variable.
-        .EXAMPLE
-        PS> Add-Env-Variable -envName path -userType machine -newEnv "C:\swig"
-        To append "C:\swig" to system "path" environment variable.
-        .EXAMPLE
-        PS> Add-Env-Variable -envName path -userType user -newEnv "C:\vlc"
-        To append "C:\vlc" to user "path" environment variable.
-        .EXAMPLE
-        PS> Add-Env-Variable -envName "WORKON_HOME" -userType user -newEnv "$env:userprofile:\.virtualenvs"
-        To create "WORKON_HOME" environment variable.
-    #>
-    param(
-        [Parameter(Mandatory = $true)][string]$envName,
-        [Parameter(Mandatory = $true)][ValidateSet("user", "machine")][string]$userType,
-        [Parameter(Mandatory = $true)][string]$newEnv
-    )
-    if ($userType -eq "user") {
-        if ([System.Environment]::GetEnvironmentVariable($envname, [System.EnvironmentVariableTarget]::User).Length -eq 0) {
-            [System.Environment]::SetEnvironmentVariable($envname, $newEnv, [System.EnvironmentVariableTarget]::User)
-        }
-        else {
-            [System.Environment]::SetEnvironmentVariable($envName, [System.Environment]::GetEnvironmentVariable($envName, [System.EnvironmentVariableTarget]::User) + ";" + $newEnv, [System.EnvironmentVariableTarget]::User)
-        }
-    }
-    elseif ($userType -eq "machine") {
-        if ([System.Environment]::GetEnvironmentVariable($envname, [System.EnvironmentVariableTarget]::Machine).Length -eq 0) {
-            [System.Environment]::SetEnvironmentVariable($envname, $newEnv, [System.EnvironmentVariableTarget]::Machine)
-        }
-        else {
-            [System.Environment]::SetEnvironmentVariable($envName, [System.Environment]::GetEnvironmentVariable($envName, [System.EnvironmentVariableTarget]::Machine) + ";" + $newEnv, [System.EnvironmentVariableTarget]::Machine)
-        }
-    }
-}
-
-Import-Module posh-git
-Import-Module oh-my-posh
-Import-Module PSReadLine
-Set-PoshPrompt -Theme Avit
-Remove-item alias:wget
-Remove-item alias:curl
-
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadlineKeyHandler -Key Ctrl+Tab -Function TabCompleteNext
-Set-PSReadlineKeyHandler -Key Ctrl+Shift+Tab -Function TabCompletePrevious
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-Set-PSReadLineKeyHandler -Key 'Ctrl+"', "Ctrl+'" `
-    -BriefDescription SmartInsertQuote `
-    -LongDescription "Insert paired quotes if not already on a quote" `
-    -ScriptBlock {
-    param($key, $arg)
-
-    $quote = $key.KeyChar
-
-    $selectionStart = $null
-    $selectionLength = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
-
-    $line = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-    # If text is selected, just quote it without any smarts
-    if ($selectionStart -ne -1) {
-        [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $quote + $line.SubString($selectionStart, $selectionLength) + $quote)
-        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
-        return
-    }
-
-    $ast = $null
-    $tokens = $null
-    $parseErrors = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$parseErrors, [ref]$null)
-
-    function FindToken {
-        param($tokens, $cursor)
-
-        foreach ($token in $tokens) {
-            if ($cursor -lt $token.Extent.StartOffset) { continue }
-            if ($cursor -lt $token.Extent.EndOffset) {
-                $result = $token
-                $token = $token -as [StringExpandableToken]
-                if ($token) {
-                    $nested = FindToken $token.NestedTokens $cursor
-                    if ($nested) { $result = $nested }
-                }
-
-                return $result
-            }
-        }
-        return $null
-    }
-
-    $token = FindToken $tokens $cursor
-
-    # If we're on or inside a **quoted** string token (so not generic), we need to be smarter
-    if ($token -is [StringToken] -and $token.Kind -ne [TokenKind]::Generic) {
-        # If we're at the start of the string, assume we're inserting a new string
-        if ($token.Extent.StartOffset -eq $cursor) {
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote ")
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-            return
-        }
-
-        # If we're at the end of the string, move over the closing quote if present.
-        if ($token.Extent.EndOffset -eq ($cursor + 1) -and $line[$cursor] -eq $quote) {
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-            return
-        }
-    }
-
-    if ($null -eq $token -or
-        $token.Kind -eq [TokenKind]::RParen -or $token.Kind -eq [TokenKind]::RCurly -or $token.Kind -eq [TokenKind]::RBracket) {
-        if ($line[0..$cursor].Where{ $_ -eq $quote }.Count % 2 -eq 1) {
-            # Odd number of quotes before the cursor, insert a single quote
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
-        }
-        else {
-            # Insert matching quotes, move cursor to be in between the quotes
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote")
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-        }
-        return
-    }
-
-    # If cursor is at the start of a token, enclose it in quotes.
-    if ($token.Extent.StartOffset -eq $cursor) {
-        if ($token.Kind -eq [TokenKind]::Generic -or $token.Kind -eq [TokenKind]::Identifier -or 
-            $token.Kind -eq [TokenKind]::Variable -or $token.TokenFlags.hasFlag([TokenFlags]::Keyword)) {
-            $end = $token.Extent.EndOffset
-            $len = $end - $cursor
-            [Microsoft.PowerShell.PSConsoleReadLine]::Replace($cursor, $len, $quote + $line.SubString($cursor, $len) + $quote)
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($end + 2)
-            return
-        }
-    }
-
-    # We failed to be smart, so just insert a single quote
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
-}
-
-Set-PSReadLineKeyHandler -Key "Alt+'" `
-    -BriefDescription ToggleQuoteArgument `
-    -LongDescription "Toggle quotes on the argument under the cursor" `
-    -ScriptBlock {
-    param($key, $arg)
-
-    $ast = $null
-    $tokens = $null
-    $errors = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
-
-    $tokenToChange = $null
-    foreach ($token in $tokens) {
-        $extent = $token.Extent
-        if ($extent.StartOffset -le $cursor -and $extent.EndOffset -ge $cursor) {
-            $tokenToChange = $token
-
-            # If the cursor is at the end (it's really 1 past the end) of the previous token,
-            # we only want to change the previous token if there is no token under the cursor
-            if ($extent.EndOffset -eq $cursor -and $foreach.MoveNext()) {
-                $nextToken = $foreach.Current
-                if ($nextToken.Extent.StartOffset -eq $cursor) {
-                    $tokenToChange = $nextToken
-                }
-            }
-            break
-        }
-    }
-
-    if ($tokenToChange -ne $null) {
-        $extent = $tokenToChange.Extent
-        $tokenText = $extent.Text
-        if ($tokenText[0] -eq '"' -and $tokenText[-1] -eq '"') {
-            # Switch to no quotes
-            $replacement = $tokenText.Substring(1, $tokenText.Length - 2)
-        }
-        elseif ($tokenText[0] -eq "'" -and $tokenText[-1] -eq "'") {
-            # Switch to double quotes
-            $replacement = '"' + $tokenText.Substring(1, $tokenText.Length - 2) + '"'
-        }
-        else {
-            # Add single quotes
-            $replacement = "'" + $tokenText + "'"
-        }
-
-        [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
-            $extent.StartOffset,
-            $tokenText.Length,
-            $replacement)
-    }
-}
-'@
+Copy-Item -Path microsoft.powershell_profile.ps1 - Destination $HOME\documents\windowspowershell\microsoft.powershell_profile.ps1
+# New-Item -Path $HOME\documents\windowspowershell\microsoft.powershell_profile.ps1 -ItemType File
+# Add-Content $HOME\documents\windowspowershell\microsoft.powershell_profile.ps1 @'
+# strings goes here
+# '@
 
 # Download Windows 10 App Package
 # Download WinGet
