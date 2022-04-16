66ee30ea3e2ef2938e40930f9dc2def425e8e862
Muhammad Hafiz <mhafiz87@users.noreply.github.com>
2022-04-16 09:56:07 +0800

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -87,3 +87,54 @@ Function Add-Env-Variable {
         }
     }
 }
+
+function Save-Download {
+    <#
+    .SYNOPSIS
+        Given a the result of WebResponseObject, will download the file to disk without having to specify a name.
+    .DESCRIPTION
+        Given a the result of WebResponseObject, will download the file to disk without having to specify a name.
+    .PARAMETER WebResponse
+        A WebResponseObject from running an Invoke-WebRequest on a file to download
+    .EXAMPLE
+        # Download Microsoft Edge
+        $download = Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2109047&Channel=Stable&language=en&consent=1"
+        $download | Save-Download 
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline)]
+        [Microsoft.PowerShell.Commands.WebResponseObject]
+        $WebResponse,
+
+        [Parameter(Mandatory = $false)]
+        [string]
+        $Directory = "."
+    )
+
+    $errorMessage = "Cannot determine filename for download."
+
+    if (!($WebResponse.Headers.ContainsKey("Content-Disposition"))) {
+        Write-Error $errorMessage -ErrorAction Stop
+    }
+
+    $content = [System.Net.Mime.ContentDisposition]::new($WebResponse.Headers["Content-Disposition"])
+    
+    $fileName = $content.FileName
+
+    if (!$fileName) {
+        Write-Error $errorMessage -ErrorAction Stop
+    }
+
+    if (!(Test-Path -Path $Directory)) {
+        New-Item -Path $Directory -ItemType Directory
+    }
+    
+    $fullPath = Join-Path -Path $Directory -ChildPath $fileName
+
+    Write-Verbose "Downloading to $fullPath"
+
+    $file = [System.IO.FileStream]::new($fullPath, [System.IO.FileMode]::Create)
+    $file.Write($WebResponse.Content, 0, $WebResponse.RawContentLength)
+    $file.Close()
+}
