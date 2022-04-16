0b7558b0c03582814f7ec8dd49a146cfd46d67ea
Muhammad Hafiz <mhafiz87@users.noreply.github.com>
2022-04-16 10:18:52 +0800

    Update install_software.ps1

--- install_software.ps1
+++ install_software.ps1
@@ -1,5 +1,6 @@
 # TODO: Add parameter for directory
 
 Invoke-WebRequest -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user" | Save-Download -Directory "$env:userprofile\Software"
+Invoke-WebRequest -Uri "https://get.videolan.org/vlc/3.0.16/win64/vlc-3.0.16-win64.exe" -OutFile "$env:userprofile\Software\vlc-3.0.16-win64.exe"
 
 # TODO: Add additional software
